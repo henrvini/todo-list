a4c0c8fe15b5a38465dbb06bc2aa38609e85b42d
Vinicius Rodrigues <vini0593@gmail.com>
2022-10-10 15:11:22 -0300

    Javascript structure

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const Main = {
+  init: function () {
+    this.cacheSelectors();
+    this.bindEvents();
+  },
+  cacheSelectors: function () {
+    this.checkButtons = document.querySelectorAll(".check");
+  },
+  bindEvents: function () {
+    const self = this;
+
+    this.checkButtons.forEach(function (button) {
+      button.onclick = self.Events.checkButton_click;
+    });
+  },
+  Events: {
+    checkButton_click: function () {
+      alert("ok");
+    },
+  },
+};
+
+Main.init();
